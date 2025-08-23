13d0d2031bda3b3b43c257229a07e393f8004c13
jasoncarty <carty.jason@gmail.com>
2025-08-23 18:03:09 +0200

    deletes unused files

--- ML_Webserver/automated_retraining_pipeline.py
+++ ML_Webserver/automated_retraining_pipeline.py
@@ -18,7 +18,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('automated_retraining.log'),
+        logging.FileHandler('logs/automated_retraining.log'),
         logging.StreamHandler()
     ]
 )
